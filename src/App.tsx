8fb733fa541a4cd2f93a6daece292ab320004a68
Erielcio Imamura <erielcio00@gmail.com>
2022-01-30 09:47:42 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 1
         </a>
         <Button color="primary">Hello World</Button>
       </header>
